bbd4d21ff1cdce6406d9bf4d712d7f83c6be84c3
bosus012 <136705396+bosus012@users.noreply.github.com>
2023-07-16 09:46:10 +0530

    Add files via upload

---
+++ 5_reg.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+void main()
+{
+char a[20];
+int i,j=0;
+FILE *f;
+f=fopen("z.txt","r");
+while(fscanf(f,"%s",a)!=EOF)
+{
+if(a[3]=='-')//first if
+{
+printf("MOV R%d,%c\n",j,a[4]);
+printf("NEG R%d\n",j);
+j++;
+}
+if(isalpha(a[3]))//second if
+{
+if(isdigit(a[4]))
+{
+if(isdigit(a[5]=='+'))
+printf("ADD R%d,R%d,R%d\n",j,j-4,j-1);
+
+else if(a[5]=='-')
+printf("SUB R%d,R%d,R%d\n",j,j-4,j-1);
+else if(a[5]=='*')
+printf("MUL R%d,R%d,R%d\n",j,j-4,j-1);
+else if(a[5]=='/')
+printf("DIV R%d,R%d,R%d\n",j,j-4,j-1);
+}
+else
+{
+if(a[4]=='+')
+{
+printf("MOV R%d,%c\n",j++,a[3]);
+printf("MOV R%d,%c\n",j++,a[5]);
+printf("ADD R%d,R%d,R%d\n",j,j-2,j-1);
+}
+if(a[4]=='-')
+{
+printf("MOV R%d,%c\n",j++,a[3]);
+printf("MOV R%d,%c\n",j++,a[5]);
+printf("SUB R%d,R%d,R%d\n",j,j-2,j-1);
+}
+else if(a[4]=='*')
+{
+printf("MOV R%d,%c\n",j++,a[3]);
+printf("MOV R%d,%c\n",j++,a[5]);
+printf("MUL R%d,R%d,R%d\n",j,j-2,j-1);
+}
+else if(a[4]=='/')
+{
+printf("MOV R%d,%c\n",j++,a[3]);
+printf("MOV R%d,%c\n",j++,a[5]);
+printf("DIV R%d,R%d,R%d\n",j,j-2,j-1);
+}
+j++;
+}//end else
+}//end isalpha
+if(isdigit(a[3]))//third if
+{
+printf("MOV %c,R%d\n",a[0],j);
+}//end while
+
+}//end main
